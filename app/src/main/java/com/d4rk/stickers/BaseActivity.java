21b63cbd495cf270768971f7b48bbad99f961afa
D4rK <d4rk7355608@gmail.com>
2020-09-28 16:11:26 +0300

    Add all my files

---
+++ app/src/main/java/com/d4rk/stickers/BaseActivity.java
@@ -0,0 +1,58 @@
+/*
+ * Copyright (c) WhatsApp Inc. and its affiliates.
+ * All rights reserved.
+ *
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+package com.d4rk.stickers;
+
+import android.app.Dialog;
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.StringRes;
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.DialogFragment;
+
+public abstract class BaseActivity extends AppCompatActivity {
+    @Override
+    public boolean onSupportNavigateUp() {
+        onBackPressed();
+        return true;
+    }
+
+    public static final class MessageDialogFragment extends DialogFragment {
+        private static final String ARG_TITLE_ID = "title_id";
+        private static final String ARG_MESSAGE = "message";
+
+        static DialogFragment newInstance(String message) {
+            DialogFragment fragment = new MessageDialogFragment();
+            Bundle arguments = new Bundle();
+            arguments.putInt(ARG_TITLE_ID, R.string.title_validation_error);
+            arguments.putString(ARG_MESSAGE, message);
+            fragment.setArguments(arguments);
+            return fragment;
+        }
+
+        @NonNull
+        @Override
+        public Dialog onCreateDialog(Bundle savedInstanceState) {
+            assert getArguments() != null;
+            @StringRes final int title = getArguments().getInt(ARG_TITLE_ID);
+            String message = getArguments().getString(ARG_MESSAGE);
+
+            AlertDialog.Builder dialogBuilder = new AlertDialog.Builder(requireActivity())
+                    .setMessage(message)
+                    .setCancelable(true)
+                    .setPositiveButton(android.R.string.ok, (dialog, which) -> dismiss());
+
+            if (title != 0) {
+                dialogBuilder.setTitle(title);
+            }
+            return dialogBuilder.create();
+        }
+    }
+}
